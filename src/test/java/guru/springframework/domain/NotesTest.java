af6680a618846f854ad231ebcdd02ca9e352f3f3
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-02 17:24:20 +0300

    Adds dependencies for JUnit 5

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NotesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+}
